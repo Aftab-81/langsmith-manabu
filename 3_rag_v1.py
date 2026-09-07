7f0b050a7ac329fa5fbdf817014f2569d12285c9
Aftabalam Makandar <makandaraftabalam@gmail.com>
2026-09-07 10:52:56 +0530

    Updated 3_rag_v1.py

--- 3_rag_v1.py
+++ 3_rag_v1.py
@@ -3,10 +3,12 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 
@@ -23,7 +25,8 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 splits = splitter.split_documents(docs)
 
 # 3) Embed + index
-emb = OpenAIEmbeddings(model="text-embedding-3-small")
+emb = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
 vs = FAISS.from_documents(splits, emb)
 retriever = vs.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
@@ -34,7 +37,15 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 
 # 5) Chain
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Flash",
+    task="text-generation",
+    max_new_tokens = 1024
+)
+
+model = ChatHuggingFace(llm=llm)
+
 def format_docs(docs): return "\n\n".join(d.page_content for d in docs)
 
 parallel = RunnableParallel({
@@ -42,7 +53,7 @@ parallel = RunnableParallel({
     "question": RunnablePassthrough()
 })
 
-chain = parallel | prompt | llm | StrOutputParser()
+chain = parallel | prompt | model | StrOutputParser()
 
 # 6) Ask questions
 print("PDF RAG ready. Ask a question (or Ctrl+C to exit).")
